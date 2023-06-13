9e8fd97d89f281e4893d3723f04fecbfca6abfb0
GlobalVentures.com <maidabarrientos@gmail.com>
2023-06-13 20:25:11 +0800

    Add files via upload

---
+++ precache-manifest.5567285ca1fc618592695a8f2e46b6f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a9cd006698eb573c0b61c19597d188",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05b7b633fde1e48c0f85",
+    "url": "/static/css/2.2980fdce.chunk.css"
+  },
+  {
+    "revision": "0473b5aa0aeb0cd3de9e",
+    "url": "/static/css/main.15e762cf.chunk.css"
+  },
+  {
+    "revision": "05b7b633fde1e48c0f85",
+    "url": "/static/js/2.18c8f594.chunk.js"
+  },
+  {
+    "revision": "df1f5ab659670c2bdb639dfcb1a2874a",
+    "url": "/static/js/2.18c8f594.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0473b5aa0aeb0cd3de9e",
+    "url": "/static/js/main.27957e4e.chunk.js"
+  },
+  {
+    "revision": "34e11a1c7a6ea85fbf05",
+    "url": "/static/js/runtime-main.565bec5e.js"
+  }
+]);
